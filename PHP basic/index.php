0783b18dce9ac8106685dc3ebf6d3b8b0f5c6c06
Emil <emil88pl@gmail.com>
2017-03-06 10:29:28 +0000

    Add files via upload

---
+++ PHP basic/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+<meta charset="utf-8">
+	<title>PHP</title>
+</head>
+
+<body>
+
+<?php
+
+	$name="Frankie";
+
+
+echo $name.",hello";
+
+?>
+
+</body>
+</html>
