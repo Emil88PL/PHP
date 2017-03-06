813929c3d61d482d99b6e2b9e6d4c066746c190c
Emil <emil88pl@gmail.com>
2017-03-06 10:30:00 +0000

    Add files via upload

---
+++ PHP LogIn/connect.php
@@ -0,0 +1,8 @@
+<?php
+
+$hot="localhost";
+$db_user="root";
+$db_password="";
+$db_name="players";
+
+?>
---
+++ PHP LogIn/game.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['logged'])){
+	header('Location: index.php');    //this go everywhere 
+	exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<title>Game</title>
+</head>
+
+<body>
+“A reader lives a thousand lives before he dies, said Jojen. The man who never reads lives only one.”
+
+<?php
+
+echo"<p>Witaj ".$_SESSION['user'].'![<a href="logout.php">Sign out</a>]</p>';
+echo"<p><b>Drewno</b>: ".$_SESSION['drewno'];
+echo"| <b>Kamien</b>: ".$_SESSION['kamien'];
+echo"| <b>Zboze</b>: ".$_SESSION['zboze']."</p>";
+
+echo"<p><b>E-mail</b>: ".$_SESSION['email'];
+echo"<br/><b>Dni premium</b> ".$_SESSION['dnipremium']."</p>";
+
+?>
+
+
+</form>
+
+</body>
+</html>
